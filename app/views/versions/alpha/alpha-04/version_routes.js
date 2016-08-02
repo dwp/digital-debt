b29150844bc29a4633e85f1d52ca7df9e4c4744f
Darren Silcox <dsilcox.digitaldwp@gmail.com>
2016-08-02 10:23:27 +0100

    Develop reduce payment page

--- app/views/versions/alpha/alpha-04/version_routes.js
+++ app/views/versions/alpha/alpha-04/version_routes.js
@@ -8,6 +8,28 @@ module.exports = function(router, config) {
         
     // place version routing below this line:
     // console.log(requestedPage);
+
+    switch(requestedPage) {
+
+
+    	// Self employed main page  
+      case 'reduce_value':
+      	if(postData['benefits'] == 'weekly') {
+          res.redirect('pay_weekly');
+        }
+        else if (postData['benefits'] == 'fortnightly') {
+          res.redirect('pay_fortnightly');
+        } 
+        else if (postData['benefits'] == 'four-weekly') {
+          res.redirect('pay_four-weekly');
+        } 
+        else if (postData['benefits'] == 'monthly') {
+          res.redirect('pay_monthly');
+        }
+
+        break;
+
+    }
     
     next();
   
