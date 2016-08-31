f55f064d670957f3c517412c88e9d24351ad0241
Paul Smith <mail@airfront.co.uk>
2016-08-31 09:14:53 +0100

    created basis for the refactored version and moves JS out of shared layout into version specific one

---
+++ app/views/versions/alpha/alpha-refactor/version_routes.js
@@ -0,0 +1,33 @@
+module.exports = function(router, config) {
+  
+  // routing for all pages directly below version/app/
+  router.all(config.routes.step, function(req,res,next){
+
+    var requestedPage = req.params.step,
+        postData = req.body || {};
+        
+    // place version routing below this line:
+    // console.log(requestedPage);
+
+    switch(requestedPage) {
+
+
+    // How often 
+      case 'increase_value':
+      	if(postData['increase'] == 'increase-v') {
+          res.redirect('increase_value');
+        }
+        else if (postData['increase'] == 'decrease-v') {
+          res.redirect('reduce_value');
+        } 
+
+        break;
+
+    }
+    
+    next();
+  
+  });
+
+  return router;
+}
