dd8c626238b5feabeee1db708fda759bfcfa95f9
gavinelliott <gavin@gavinelliott.co.uk>
2016-10-03 09:08:47 +0100

    Change of service name

--- app/config.js
+++ app/config.js
@@ -5,7 +5,7 @@
 module.exports = {
 
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: "DWP Debt",
+  serviceName: "Check your benefits",
 
   // Default port that prototype runs on
   port: '3000',
